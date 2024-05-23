b2bc4834aa99ca2f6e65726816befb81a965f3af
jaxayM <missour1998@gmail.com>
2024-05-23 17:46:10 +0200

    init commmand

--- app/main.py
+++ app/main.py
@@ -7,17 +7,24 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this block to pass the first stage
-    #
-    # command = sys.argv[1]
-    # if command == "init":
-    #     os.mkdir(".git")
-    #     os.mkdir(".git/objects")
-    #     os.mkdir(".git/refs")
-    #     with open(".git/HEAD", "w") as f:
-    #         f.write("ref: refs/heads/main\n")
-    #     print("Initialized git directory")
-    # else:
-    #     raise RuntimeError(f"Unknown command #{command}")
+    # args: list[str] = sys.argv
+    # if (args[1] == "init"):
+    #     os.makedirs(".git/objects")
+    #     os.makedirs(".git/refs")
+    #     f = open(".git/HEAD", 'w')
+    #     f.write("ref: refs/heads/main\n")
+    #     f.close()
+    
+    command = sys.argv[1]
+    if command == "init":
+        os.mkdir(".git")
+        os.mkdir(".git/objects")
+        os.mkdir(".git/refs")
+        with open(".git/HEAD", "w") as f:
+            f.write("ref: refs/heads/main\n")
+        print("Initialized git directory")
+    else:
+        raise RuntimeError(f"Unknown command #{command}")
 
 
 if __name__ == "__main__":
